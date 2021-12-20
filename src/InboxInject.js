8fdc6f47599a5f7c3b4e21510bb8acbb90014594
pambonian <pambonian@gmail.com>
2021-12-20 12:44:45 -0500

    same thing repeat component

--- src/InboxInject.js
+++ src/InboxInject.js
@@ -21,7 +21,7 @@ class InboxInject extends Component {
                             <span className="msg-subject"><strong id="fake-subject-1">{}</strong></span>
                         </div>
                         <div className="msg-snippet">
-                            <p id="fake-snippet-1">{this.props.message}Ea totam sunt nobis rerum. Id qui aliquam neque aliquam fugiat nihil. Dolores blanditiis placeat.</p>
+                            <p id="fake-snippet-1">{this.props.message}</p>
                         </div>
                     </div>
                 </div>
