fa9b770fde475da5fc8172a9183fe74e550bf865
pambonian <pambonian@gmail.com>
2021-12-20 17:23:02 -0500

    changes

--- src/InboxInject.js
+++ src/InboxInject.js
@@ -7,25 +7,17 @@ class InboxInject extends Component {
     render() {
 
         return (
-        <div>
-        <div className="post" key={this.props.index} />
-        <div className="inbox-messages" id="inbox-messages">
-                <div v-for="(msg, index) in messages" className="card">
-                    <div className="card-content">
-                        <div className="msg-header">
-                            <span className="msg-from"><small>Player: {this.props.person}</small></span>
-                            <span className="msg-timestamp"></span>
-                            <span className="msg-attachment"><i className="fa fa-paperclip"></i></span>
-                        </div>
-                        <div className="msg-subject">
-                            <span className="msg-subject"><strong id="fake-subject-1">{}</strong></span>
-                        </div>
-                        <div className="msg-snippet">
-                            <p id="fake-snippet-1">{this.props.message}</p>
-                        </div>
-                    </div>
-                </div>
-                
+            <div className="card-content" key={this.props.index}>
+            <div className="msg-header">
+                <span className="msg-from"><small>{this.props.person}</small></span>
+                <span className="msg-timestamp"></span>
+                <span className="msg-attachment"><i className="fa fa-paperclip"></i></span>
+            </div>
+            <div className="msg-subject">
+                <span className="msg-subject"><strong id="fake-subject-1">{}</strong></span>
+            </div>
+            <div className="msg-snippet">
+                <p id="fake-snippet-1">{box.message}{this.props.message}</p>
             </div>
         </div>
         );
@@ -33,3 +25,25 @@ class InboxInject extends Component {
     }
    
     export default InboxInject;
+
+    // <div>
+    //     // <div className="post" key={this.props.index} />
+    //     // <div className="inbox-messages" id="inbox-messages">
+    //     //         <div v-for="(msg, index) in messages" className="card">
+    //     //             <div className="card-content">
+    //     //                 <div className="msg-header">
+    //     //                     <span className="msg-from"><small>Player: {this.props.person}</small></span>
+    //     //                     <span className="msg-timestamp"></span>
+    //     //                     <span className="msg-attachment"><i className="fa fa-paperclip"></i></span>
+    //     //                 </div>
+    //     //                 <div className="msg-subject">
+    //     //                     <span className="msg-subject"><strong id="fake-subject-1">{}</strong></span>
+    //     //                 </div>
+    //     //                 <div className="msg-snippet">
+    //     //                     <p id="fake-snippet-1">{this.props.message}</p>
+    //     //                 </div>
+    //     //             </div>
+    //     //         </div>
+                
+    //     //     </div>
+    //     // </div>
